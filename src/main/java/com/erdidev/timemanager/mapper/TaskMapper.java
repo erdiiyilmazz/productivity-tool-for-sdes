d63836924d767c671232bdd019a9f23670eee6f3
erdiiyilmazz <erdiyilmaz90@gmail.com>
2025-01-15 15:05:40 +0300

    unmapped fields added to task mapper

--- src/main/java/com/erdidev/timemanager/mapper/TaskMapper.java
+++ src/main/java/com/erdidev/timemanager/mapper/TaskMapper.java
@@ -3,11 +3,18 @@ package com.erdidev.timemanager.mapper;
 import com.erdidev.timemanager.dto.TaskDto;
 import com.erdidev.timemanager.model.Task;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper
 public interface TaskMapper {
     TaskDto toDto(Task task);
+
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     Task toEntity(TaskDto taskDto);
+
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     void updateEntity(TaskDto taskDto, @MappingTarget Task task);
 } 
